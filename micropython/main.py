2cbcf39605c279c33e2e659c9e70ca047e997c03
Matheus Henrique <matheus.mhsantos@mercadolivre.com>
2024-11-14 21:56:18 -0300

    feat: final

--- micropython/main.py
+++ micropython/main.py
@@ -1,40 +1,76 @@
-# Importando bibliotecas 
 import os # Para o leitura de aquivo CSV
 import time # Para o controle de tempo de leitura
 import network # Para a conexão wi-fi
-import urequests as requests
+import urequests
+import ujson as json
 import dht #Para o sensor dht11
-import machine # Para o hasp
+import machine   # Para o hasp
+from umqtt.robust import MQTTClient
 
 BASE_URL_POST_SYNC = "http://127.0.0.1:8000/v1/save"
 BASE_URL_POST_CSV = "http://127.0.0.1:8000/v1/save_csv"
 headers = {'Content-Type': 'application/json'}
 
+# Constantes para o ThingSpeak
+WRITE_API_KEY = "YDZDO7GEE8L3Y38Q"  # Insira sua chave de API do ThingSpeak
+THINGSPEAK_WRITE_URL = "https://api.thingspeak.com/update?api_key=YDZDO7GEE8L3Y38Q&field1=0"
+THINGSPEAK_USER_ID = b"quii" # Idendificacao do usuario no ThingSpeak
+THINGSPEAK_FIELD1 = "field1"
+THINGSPEAK_FIELD2 = "field2"
+
+# Configurações do MQTT
+MQTT_BROKER = "mqtt.thingspeak.com"
+MQTT_USER = "seu_usuario"  # Substitua pelo seu ThingSpeak User ID
+MQTT_API_KEY = "sua_chave_api"  # Substitua pela sua chave de API do ThingSpeak
+MQTT_CLIENT_ID = "client_id_unico"  # Um ID único para o cliente MQTT
+
 # Login e senha do WI-FI
-wifi_login = 'RNS'
-wifi_password = 'VaiFuncionar'
+wifi_login = "Wokwi-GUEST"
+wifi_password = ""
 
 # Arquivo para salvar dados temporários
-file_name = 'data_logger.csv'
+file_name = "data_logger.csv"
+
+# Definindo onde está o sensor  
+sensor = dht.DHT11(machine.Pin(34))
+wlan = network.WLAN(network.STA_IF)  # Declarando conexão
      
 # Conexão wifi --- tem que testar no rasp          
 def connect_wifi():
-    wlan = network.WLAN(network.STA_IF) # Declarando conexão
-    wlan.active(True) # Ativando conexão
-    wlan.connect(wifi_login, wifi_password) # Usando login e senha
-    while not wlan.isconnected(): # Enquanto não conectar 
-        print("Conectando...")
-        time.sleep(1) # Espera um segundo  
-    print('WiFi conectado:', wlan.ifconfig())
+    wlan.active(True)  # Ativando conexão
+    wlan.connect(wifi_login, wifi_password)  # Usando login e senha
+    print(f'Conectando à rede {wifi_login}.', end="")
+    while not wlan.isconnected():  # Enquanto não conectar 
+        print(".", end="")  # Imprime um ponto sem nova linha
+        time.sleep(0.3)  # Espera meio segundo
+    print('WiFi Conectado!')
+    print("Infos:",  wlan.ifconfig())
 
 # Função para verificar a conexão Wi-Fi
 def is_connected():
     wlan = network.WLAN(network.STA_IF)
     return wlan.isconnected()
 
-# Definindo onde está o sensor  
-sensor = dht.DHT11(machine.Pin(26))
-print(sensor)
+# Função para conectar ao MQTT
+def connect_mqtt():
+    client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_API_KEY)
+    try:
+        client.connect()
+        print("Conectado ao MQTT Broker:", MQTT_BROKER)
+        return client
+    except Exception as e:
+        print("Erro ao conectar ao MQTT:", e)
+        return None
+
+# Cria um clientID MQTT aleatorio
+randomNum = int.from_bytes(os.urandom(3), "little")
+myMqttClient = bytes("client_"+str(randomNum), "utf-8")
+# Conecta ao Thingspeak MQTT broker
+# Conexão TCP (port 1883) - Não oferece segurança.
+# Para uma conexão segura utilizaremos TLS (Transport Layer Security) - protocolo de Criptografia
+# Ajustando o parâmetro inicializador MQTTClient para "ssl=True" - SSL
+# Observação: Uma conexão segura utiliza aproximadamente 9 kB da pilha de memoria
+
 
 # Lendo informações de umidade e de temperatura
 def read_humidity_temperature():
@@ -43,8 +79,11 @@ def read_humidity_temperature():
         time.sleep(0.5)
         humidity = sensor.humidity() # Pega dados de umidade em porcentagem
         temperature = sensor.temperature() # Pega dados de temperatura em Celsius
-        print(humidity, temperature)
-        return humidity, temperature # Retorna umidade e temperatura
+        # Capturar a data e a hora atual (data da Leitura)
+        date_time = time.localtime()
+        reading_date = '{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(date_time[0], date_time[1], date_time[2],date_time[3], date_time[4], date_time[5])
+
+        return humidity, temperature, reading_date # Retorna umidade, temperatura e data
     except OSError as e:
         print("Erro ao ler o sensor:", e)
         time.sleep(1)  # Espera um segundo antes de tentar novamente
@@ -103,6 +142,7 @@ def delete_csv_file():
         print("Arquivo não encontrado para eliminação.")   
 
 def send_data_sync():
+    print("AQUII SYNC SAVE")
     try:        
         humidity, temperature = read_humidity_temperature()
         data = {
@@ -114,7 +154,7 @@ def send_data_sync():
                }
            
         print(data)  
-        response = requests.post(BASE_URL_POST_SYNC, json=data, headers=headers)
+        response = urequests.post(BASE_URL_POST_SYNC, json=data, headers=headers)
         
         if response.status_code == 200:
             print('Dados enviados com sucesso!')
@@ -122,22 +162,27 @@ def send_data_sync():
             print('Erro ao enviar dados:', response.status_code)
         response.close()
     except Exception as e:
-        print('Erro ao enviar dados:', e)
+        print('Exception Erro ao enviar dados:', e)
 
 # Função para enviar dados do CSV quando a conexão for restabelecida
 def send_csv_data():
     if file_exists(file_name):
         with open(file_name, 'r') as file:
             data = file.read()
-        response = requests.post(BASE_URL_POST_CSV, headers=headers, data=data, timeout=10)
-        if response.status_code == 200:
-            print('Dados do CSV enviados com sucesso!')
-            delete_csv_file() # Remove o arquivo após o envio
-        else:
-            print('Erro ao enviar dados do CSV:', response.status_code)
+        try:
+            response = urequests.post(BASE_URL_POST_CSV, headers=headers, json=data, timeout=10)
+            if response.status_code == 200:
+                print('Dados do CSV enviados com sucesso!')
+                delete_csv_file() # Remove o arquivo após o envio
+            else:
+                print('Erro ao enviar dados do CSV:', response.status_code)
+        except Exception as e:
+            print('Falha ao enviar dados:', e)
 
 # Iniciando programa
 connect_wifi()
+mqtt_client = connect_mqtt()
+print("MQQTTTT: ", mqtt_client)
 while True:
     humidity_temperature = read_humidity_temperature()  # Lê os dados do sensor
     if is_connected():  # Verifica se está conectado ao Wi-Fi
@@ -147,5 +192,6 @@ while True:
         time.sleep(10)  # Esperar 10 segundos para nova leitura
         send_csv_data()  # Tenta enviar dados do CSV se a conexão for restabelecida
 
+    time.sleep(1)  # Esperar 1 segundos para nova leitura
     print(' ')
     read_csv_data()
